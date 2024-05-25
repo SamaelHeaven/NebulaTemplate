5df1b5f64632884cddb98f91ea2d3c02d16be402
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-25 03:31:37 -0400

    Fix: Minor fix

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,15 +1,15 @@
 package org.example.app;
 
-import org.samaelheaven.nebula.core.Application;
+import org.samaelheaven.nebula.core.BaseApplication;
 import org.samaelheaven.nebula.core.Kernel;
 import org.samaelheaven.nebula.network.Request;
 import org.samaelheaven.nebula.network.Response;
 
 import java.nio.charset.StandardCharsets;
 
-public final class WebApplication extends Application {
+public final class Application extends BaseApplication {
     public static void main(String[] args) {
-        Kernel.launch(new WebApplication());
+        Kernel.launch(new Application());
     }
 
     @Override
